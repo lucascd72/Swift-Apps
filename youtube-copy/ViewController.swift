79284d625d87594f97c8f98b0f97aeed587a0dd7
Lucas Danelon <lucascd72@hotmail.com>
2020-06-09 08:38:47 +0100

    initial xcode project creation

---
+++ youtube-copy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-copy
+//
+//  Created by Lucas Danelon on 09/06/2020.
+//  Copyright © 2020 Lucas Danelon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
